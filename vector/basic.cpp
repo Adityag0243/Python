37bad715dbe565b45bffc2fc05f2816dfba36b14
Aditya Gupta <mr.adityag123@gmail.com>
2024-04-01 10:29:32 +0530

    uploading all previous thing

---
+++ vector/basic.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	for(int i=0;i<t;i++){
+	    int n,h;
+	    cin>>n>>h;
+	    vector<int> v(n);
+	    for(int j=0;j<n;j++){
+	        cin>>v.at(j);
+	    }
+	    v.sort(begin.with().end.with());
+	    
+	    for(int j=0;j<n;j++){
+	        cout<<v[j];
+	    }
+	}
+
+}
